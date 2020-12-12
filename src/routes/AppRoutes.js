961c09bf987bc7a27ffab7c98babf997a0453082
EnderLab <enderlabo@gmail.com>
2020-12-11 21:13:41 -0500

    update

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { useSelector, useDispatch } from 'react-redux'
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect, HashRouter } from "react-router-dom";
 import  Counter from '../components/Counter/Counter';
 import { Register } from '../components/Auth/Register';
 import { Molitalia } from '../components/Home/Molitalia';
@@ -15,22 +15,20 @@ export const AppRoutes= () => {
           <div>
             <Switch>
                 {/* Protected routes with Auth */}
-                
                 {/* {
                   isLoggedIn ? ( */}
-                    <Route exact path="/mapa" component={ Molitalia } />
+                    <HashRouter basename="https://magicanavidad.molitalia.com.pe"
+                     exact path="/mapa" component={ Molitalia } />
 
-                  {/* ) :
-                  (
-                    <> */}
-                    <Route exact path="/registro" component={ Register } />
+                    <HashRouter basename="https://magicanavidad.molitalia.com.pe"
+                     exact path="/registro" component={ Register } />
                     
-                    {/* </>  
-                    )
-                  } */}
-                  <Route exact path="/contador" component={ Counter } />
+                   
+                  <HashRouter basename="https://magicanavidad.molitalia.com.pe"
+                   exact path="/contador" component={ Counter } />
 
-                  <Redirect to="/contador" />
+                  <HashRouter basename="https://magicanavidad.molitalia.com.pe"
+                   to="/contador" />
 
             </Switch>
           </div>
